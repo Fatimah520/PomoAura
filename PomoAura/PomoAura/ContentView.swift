e7cde4d28bf7ba132b8d9e3e6972a8517f48356f
Fatimah520 <51966410+Fatimah520@users.noreply.github.com>
2025-08-26 15:57:29 -0700

    Adding 1 to the button each time

--- PomoAura/PomoAura/ContentView.swift
+++ PomoAura/PomoAura/ContentView.swift
@@ -6,9 +6,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // State variable
+    @State private var count = 0
     var body: some View{
         VStack {
-            Text("Hello Timer")
+            // Displaying the number, shows the count ( which is 0)
+            Text("Count: \(count)")
+            // Start button
+            Button("Add 1") {
+                // when button is tapped, add 1
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/count += 1
+            }
+            // Pause Button
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
         }
     }
 }
